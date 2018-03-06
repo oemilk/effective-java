85e4c5b732767cafdc6aceb2883ffcf0c98281b3
pooq-shlee <shlee@captv.co.kr>
2018-03-07 08:42:20 +0900

    Add chapter03 item14

---
+++ com.sh.effectivejava/src/chapter04/item14/Point.java
@@ -0,0 +1,30 @@
+package chapter04.item14;
+
+// 접근자와 변경제 메서드를 이용한 데이터 캡슐화
+public class Point {
+
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+}
